8a978bdb47f869e54fdd9ced4406288fad86c649
reannarsb <115964061+reannarsb@users.noreply.github.com>
2023-12-06 12:26:41 -0700

    printmenu.py

--- appointment.py
+++ appointment.py
@@ -1,2 +1,8 @@
 class Appointment:
     pass
+
+def print_menu():
+    print("Jojo's Hair Salon Appointment Manager")
+    print("="*38)
+    print("1) Schedule an appointment \n2) Find appointment by name \n3) Print calendar for a specific day \n4) Cancel an appointment \n9) Exit the system")
+    selection = input("Enter your selection: ")
