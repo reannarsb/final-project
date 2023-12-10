1f78c2010b5275594a3d854c830d5ed3427e1dae
reannarsb <115964061+reannarsb@users.noreply.github.com>
2023-12-10 16:10:38 -0700

    Update appointment.py

--- appointment.py
+++ appointment.py
@@ -1,20 +1,28 @@
 class Appointment:
     pass
-
+    
 def print_menu():
+    """
+    Print the main menu for Jojo's Hair Salon Appointment Manager.
+    """
     print("Jojo's Hair Salon Appointment Manager")
     print("="*38)
     print("1) Schedule an appointment \n2) Find appointment by name \n3) Print calendar for a specific day \n4) Cancel an appointment \n9) Exit the system")
     selection = input("Enter your selection: ")
 
-
-
 def create_weekly_calendar():
-    calendar = []
-    days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', ‘Saturday’]
+    """
+    Create a weekly calendar of appointments for each day (Monday to Saturday) and each hour from 9 AM to 4 PM.
 
+    Returns:
+    list: A list of Appointment objects representing the weekly calendar.
+    """
+    calendar = []  # Initialize an empty list to store Appointment objects
+    days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+    # Loop through each day and hour to create and append Appointment objects to the calendar
     for day in days_of_the_week:
-        for hour in range(9, 17):
+        for hour in range(9, 17):  # Hours range from 9 AM to 4 PM
             calendar.append(Appointment(day, hour))
 
     return calendar
