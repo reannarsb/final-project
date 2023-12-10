a50ed61b904847883c07f6133a56cd5b531a602a
reannarsb <115964061+reannarsb@users.noreply.github.com>
2023-12-10 15:48:56 -0700

    find-appt-by-time.py

--- appointment.py
+++ appointment.py
@@ -1,2 +1,8 @@
 class Appointment:
     pass
+
+def find_appointment_by_time(appointment_list, day, start_hour):
+    for appointment in appointment_list:
+        if appointment.get_day_of_week().lower() == day.lower() and appointment.get_start_time_hour() == start_hour:
+            return appointment 
+    return None
