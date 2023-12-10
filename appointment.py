ab6186c961abe3a18ba6575ec54c17794fae3a3c
reannarsb <115964061+reannarsb@users.noreply.github.com>
2023-12-10 15:51:41 -0700

    Update appointment.py

--- appointment.py
+++ appointment.py
@@ -6,3 +6,15 @@ def find_appointment_by_time(appointment_list, day, start_hour):
         if appointment.get_day_of_week().lower() == day.lower() and appointment.get_start_time_hour() == start_hour:
             return appointment 
     return None
+
+
+def show_appointments_by_day(appointment_list, day):
+    matching_appointments = [appointment for appointment in appointment_list
+                             if appointment.get_day_of_week().lower() == day.lower()]
+
+    if matching_appointments:
+        print("Appointments on {}:".format(day))
+        for appointment in matching_appointments:
+            print(appointment)
+    else:
+        print("No appointments found on {}.".format(day))
