2577356ce6b54edf385cb23bd1ced77739133134
reannarsb <115964061+reannarsb@users.noreply.github.com>
2023-12-10 09:40:22 -0700

    calendar.py

--- appointment.py
+++ appointment.py
@@ -6,3 +6,16 @@ def print_menu():
     print("="*38)
     print("1) Schedule an appointment \n2) Find appointment by name \n3) Print calendar for a specific day \n4) Cancel an appointment \n9) Exit the system")
     selection = input("Enter your selection: ")
+
+
+
+def create_weekly_calendar():
+    calendar = []
+    days_of_the_week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', ‘Saturday’]
+
+    for day in days_of_the_week:
+        for hour in range(9, 17):
+            calendar.append(Appointment(day, hour))
+
+    return calendar
+
