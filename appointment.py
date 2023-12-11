0ba5c533a2fe7a501f4db29501a5453861ea44d2
Gabrielle Mary Flor Alama <152216187+gabriellemfa@users.noreply.github.com>
2023-12-10 17:51:23 -0700

    Update appointment.py
    added found_appointment_by_time() function

--- appointment.py
+++ appointment.py
@@ -1,6 +1,6 @@
 from appointment import Appointment  # Importing the Appointment class from your previous file
 
-def load_scheduled_appointments(filename):
+def load_scheduled_appointments(filename, calendar):
     appointments_loaded = 0
     file = open(filename, 'r')
     lines = file.readlines()
@@ -16,6 +16,17 @@ def load_scheduled_appointments(filename):
         new_appointment.set_client_name(client_name)
         new_appointment.set_client_phone(client_phone)
         new_appointment.set_appt_type(appnt_type)
+        
+        found_appointment = find_appointment_by_time(calendar, day_of_week, start_time_hour)
+        if found_appointment:
+            found_appointment.schedule(client_name, client_phone, appnt_type)
+
         appointments_loaded += 1
     
     return appointments_loaded
+
+def find_appointment_by_time(calendar, day, start_time):
+    for appt in calendar:
+        if appt.get_day_of_week() == day and appt.get_start_time_hour() == start_time:
+            return appt
+    return None
